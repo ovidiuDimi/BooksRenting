8910f722ffd4fc6ecb6d088101ddf9b9a082096f
opris dimi <ovidiu.dimi21@gmail.com>
2020-09-02 21:03:20 +0300

    Edit setup

--- BooksRenting/BooksRenting/Controllers/BooksController.cs
+++ BooksRenting/BooksRenting/Controllers/BooksController.cs
@@ -88,12 +88,16 @@ namespace BooksRenting.Controllers
         // GET: Books/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
+            
             if (id == null)
             {
                 return NotFound();
             }
 
-            var book = await _context.Books.FindAsync(id);
+            var book = new Book();
+            book.AvailableAuthors = await _context.Authors.ToListAsync();
+            book.AvailableCategories = await _context.Categories.ToListAsync();
+            book = await _context.Books.FindAsync(id);
             if (book == null)
             {
                 return NotFound();
@@ -106,7 +110,7 @@ namespace BooksRenting.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title")] Book book)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,SelectedAuthorId,SelectedCategoryId")] Book book)
         {
             if (id != book.Id)
             {
@@ -117,6 +121,19 @@ namespace BooksRenting.Controllers
             {
                 try
                 {
+                    var selectedAuthor = await _context.Authors.FirstOrDefaultAsync(a => a.Id == book.SelectedAuthorId);
+                    if (selectedAuthor is null)
+                    {
+                        ModelState.AddModelError("SelectedAuthorId", "Cannot find the Author");
+                        return View(book);
+                    }
+
+                    var selectedCategory = await _context.Categories.FirstOrDefaultAsync(c => c.Id == book.SelectedCategoryId);
+                    if (selectedCategory is null)
+                    {
+                        ModelState.AddModelError("SelectedCategoryId", "Cannot find the Category");
+                        return View(book);
+                    }
                     _context.Update(book);
                     await _context.SaveChangesAsync();
                 }
